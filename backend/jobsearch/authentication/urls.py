b4d43166e734275da7b66e780cadb9f9c2e6eed2
Najmus Sakib Dhrubo <najmussakibdhrubo52@gmail.com>
2024-09-27 23:04:34 +0600

    Worked on Register view backend

--- backend/jobsearch/authentication/urls.py
+++ backend/jobsearch/authentication/urls.py
@@ -4,9 +4,11 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
+from . import views
 
 urlpatterns = [
     path('obtain_token/', TokenObtainPairView.as_view(), name='obtain_token'),
     path('refresh_token/', TokenRefreshView.as_view(), name='refresh_token'),
     path('verify_token/', TokenVerifyView.as_view(), name='verify_token'),
+    path('register/', views.Register.as_view(), name='register'),
 ]
--- backend/jobsearch/authentication/views.py
+++ backend/jobsearch/authentication/views.py
@@ -10,22 +10,29 @@ from allmodels import UserProfile
 
 def valid_email(email):
     e = re.match('[a-z0-9]+([-._][a-z0-9]+)*@[a-z0-9]+([-][a-z0-9]+)*([.]..+)+', email)
+    if e is not None:
+        return True
+    else return False
 
 class Register(APIView):
     def post(self, request):
         email = request.data['email']
-        password1 = request.data['password1']
-        password2 = request.data['password2']
-        first_name = request.data('first_name')
-        last_name = request.data('last_name')
-        profile_type = request.data['profile_type']
+        password1 = request.data.get('password1', '')
+        password2 = request.data.get('password2', '')
+        first_name = request.data.get('first_name', '')
+        last_name = request.data.get('last_name', '')
+        profile_type = request.data.get('profile_type', '')
+
+        if valid_email(email) is None:
+            return Response({message: 'Please enter a valid email address!'}, status=status.HTTP_400_BAD_REQUEST)
 
         if User.objects.get(email=email, None):
-            return Response({message: 'A user with this email address already exists'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({message: 'A user with this email address already exists.'}, status=status.HTTP_400_BAD_REQUEST)
 
         if password1 != password2:
             return Response({message: 'Both password have to be same.'}, status=status.HTTP_400_BAD_REQUEST)
 
         user = User.obejects.create(email=email, password=make_password(password1), first_name=first_name, last_name=last_name)
-        UserProflie.objects.create(user=user, profile_type=profile_type)
+        UserProfile.objects.create(user=user, profile_type=profile_type)
 
+        return Response({message: 'Registration Successful!'}, status=status.HTTP_200_OK)
